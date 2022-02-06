e4641c98567f727c03dfb64b03f6c7914e56b522
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-02-06 18:46:22 +0500

    The covid status project completed

---
+++ corona/corona.js
@@ -0,0 +1,12 @@
+cityinput.onkeyup = element => {
+    if (element.keyCode == 13) {
+        fetch(`https://covid-19-data.p.rapidapi.com/country?name=${cityinput.value}`, {"method": "GET","headers": {	"x-rapidapi-host": "covid-19-data.p.rapidapi.com",	"x-rapidapi-key": "ab72eff8e4mshe1623af6fde16d7p1dc171jsnc3d6f0381dc4"}})
+        .then(response => response.json())
+        .then(data => {
+            if (data.length) {
+                [cityName.textContent, confirmed.textContent, recovered.textContent, critical.textContent, deaths.textContent] = [`The status of covid in ${data[0].country}`, `The confirmed: ${data[0].confirmed}`, `The recovered: ${data[0].recovered}`, `The critical: ${data[0].critical}`, `The deaths: ${data[0].deaths}`]
+            }
+        })
+        .catch(err => {alert('Wrong country name'); [cityName.textContent, confirmed.textContent, recovered.textContent, critical.textContent, deaths.textContent, cityinput.value] = [null, null, null, null, null, null]});
+    }
+}
